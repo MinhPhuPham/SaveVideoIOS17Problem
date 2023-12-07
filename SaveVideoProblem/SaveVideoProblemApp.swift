36a8b3febf428385bbc68803f3cd5f4ae6844d13
Minh Phu Pham <phamminhphueur@gmail.com>
2023-12-07 11:03:17 +0700

    Initial Commit

---
+++ SaveVideoProblem/SaveVideoProblemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveVideoProblemApp.swift
+//  SaveVideoProblem
+//
+//  Created by Phu Pham on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SaveVideoProblemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
